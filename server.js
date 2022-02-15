ad7bd3172fd68266ac78855a9b68b4bb0b883e8c
kshitij <kshtjsharma68@gmail.com>
2022-02-15 13:11:09 +0530

    server

---
+++ server.js
@@ -0,0 +1,9 @@
+//Serverjs
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/dist'));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port);
